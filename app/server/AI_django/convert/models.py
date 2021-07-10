508fb770819fbd34e7af7a336cfb764c1af0ff1b
YeonWooHwang <into1000@naver.com>
2021-07-10 15:16:39 +0900

    add web service file

---
+++ app/server/AI_django/convert/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Profile(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    
+    def __str__(self):
+        return self.title
