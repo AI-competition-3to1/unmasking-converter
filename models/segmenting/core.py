245630a4e59b64b992969a67f7b5b289defdf759
handal <handal__@naver.com>
2021-07-13 02:21:31 +0900

    fix plot image

--- models/segmenting/core.py
+++ models/segmenting/core.py
@@ -15,48 +15,29 @@ from utils.config import get_config
 from utils.dataset import MaskDataset, MaskDataLoader
 from utils.images import plot_image
 from utils.logger import Logger
+from model import get_model, get_params
 
 
 logger = Logger().get_logger()
 
 MODEL_NAME = 'Mask segmentating model'
+MODEL_PATH = "mask_segment_model.pt"
+
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-
-def main(config):
-    # Run Process
-    logger.info(f"Run `{MODEL_NAME} model` (mode: {config['mode']}) on {device}")
-
-    # Prepare Dataset
-    logger.info(f"Load Dataset from {config['data']['directory']}")
-    dataset = MaskDataset(config["data"])
-    data_loader = MaskDataLoader(config["loader"], dataset).loader
-
+def train(config, data_loader):
     # Prepare pretrained model - faster rcnn
-    NUM_CLASSES = 3
-    logger.info(f"Set Faster R-CNN model (num_classes : {NUM_CLASSES})")
-
-    model = get_model_instance_segmentation(num_classes=NUM_CLASSES)
+    model = get_model(config["model"])
     model.to(device)
 
+    # Train Model
+    EPOCHS, optimizer = get_params(config["model"], model)
+    logger.info(f"Train `{MODEL_NAME}` \n (params : {config['model']})")
 
-    logger.info(f"Load Dataset (BATCH_SIZE : {data_loader.batch_size})")
-    # for imgs, annotations in data_loader:
-    #     imgs = list(img.to(device) for img in imgs)
-    #     annotations = list({k: v.to(device) for k, v in t.items()} for t in annotations)
-
-    # parameters
-    params = [p for p in model.parameters() if p.requires_grad]
-    optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
-
-
-    num_epochs = 25
-    for epoch in range(num_epochs):
+    for epoch in range(EPOCHS):
         model.train()
-        i = 0
         epoch_loss = 0
         for imgs, annotations in data_loader:
-            i += 1
             imgs = list(img.to(device) for img in imgs)
             annotations = list({k: v.to(device) for k, v in t.items()} for t in annotations)
             loss_dict = model([imgs[0]], [annotations[0]])
@@ -66,26 +47,45 @@ def main(config):
             losses.backward()
             optimizer.step() 
             epoch_loss += losses
-        logger.info(f"Epoch [{epoch + 1}/{num_epochs}] loss {epoch_loss:.4f}")
-     
-    logger.info(model.eval())
+        logger.info(f"Epoch [{epoch + 1}/{EPOCHS}] loss {epoch_loss:.4f}")
+
     model.eval()
-    preds = model(imgs)
-
-    print("Prediction")
-    plot_image(imgs[0], preds[0])
-
-    print("Target")
-    plot_image(imgs[0], annotations[0])
-
-    print("Done")
-
-def get_model_instance_segmentation(num_classes):
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+    if config["save"]:
+        logger.info(f"Save the `{MODEL_NAME}` (path : {MODEL_PATH})")
+        torch.save(model, MODEL_PATH)
 
     return model
+
+def main(config):
+    logger.info(f"Running on {device}")
+    
+    # Prepare Dataset
+    logger.info(f"Load Dataset from {config['data']['directory']}")
+    dataset = MaskDataset(config["data"])
+    data_loader = MaskDataLoader(config["loader"], dataset).loader
+
+    logger.info(f"Model : `{MODEL_NAME}` (mode: {config['mode']}) ")
+    if config["mode"] == "train":
+        model = train(config, data_loader)
+    elif config["mode"] == "pretrain":
+        model = torch.load(MODEL_PATH)
+
+    for imgs, annotations in data_loader:
+        imgs = list(img.to(device) for img in imgs)
+        annotations = [{k: v.to(device) for k, v in t.items()} for t in annotations]
+        break
+    
+    model.eval()
+    preds = model(imgs)
+    
+    # Show results
+    for i in len(imgs):
+        if i > 10:
+            break
+        plot_image(imgs[i], preds[i])
+        plot_image(imgs[i], annotations[i])
+
+    logger.info("Process Done")
 
 if __name__ == "__main__":
     config = get_config()
--- models/segmenting/utils/images.py
+++ models/segmenting/utils/images.py
@@ -4,20 +4,23 @@ import matplotlib.patches as patches
 
 
 def plot_image(img_tensor, annotation, block=True):
-    fig,ax = plt.subplots(1)
+    fig, ax = plt.subplots(1)
+    
     img = img_tensor.cpu().data
+    annotation["boxes"] = annotation["boxes"].cpu().data 
+    annotation["labels"] = annotation["labels"].cpu().data 
+
     # Display the image
     ax.imshow( np.array( img.permute(1, 2, 0) ) )
 
     for box, label in zip( annotation["boxes"], annotation["labels"] ):
-        print("label",label)
-        print("box",box)
+        # print("label", label)
+        # print("box", box)
         xmin, ymin, xmax, ymax = box
         # Create a Rectangle patch
-        if label==1:
-            rect = patches.Rectangle((xmin,ymin),(xmax-xmin),(ymax-ymin),linewidth=1,edgecolor='b',facecolor='none')
-        else:
-            rect = patches.Rectangle((xmin,ymin),(xmax-xmin),(ymax-ymin),linewidth=1,edgecolor='r',facecolor='none')
+        edgecolor = 'b' if label == 1 else 'r'
+        rect = patches.Rectangle((xmin,ymin),(xmax-xmin),(ymax-ymin),linewidth=1,edgecolor=edgecolor,facecolor='none')
+
         # Add the patch to the Axes 
         ax.add_patch(rect)
         ax.axis("off")
