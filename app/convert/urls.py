a2dc23f255fbf9cd734ce578a5c1cf3eeb0abde6
YeonWooHwang <50245330+YeonWooHwang@users.noreply.github.com>
2021-07-17 18:30:44 +0900

    Update urls.py

--- app/convert/urls.py
+++ app/convert/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("converted/", views.convert, name="convert"),
+    path("converted/", views.unmask, name="convert"),
 ]
